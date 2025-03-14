f3d06e195063f7b5af6a2b009bba0460963dc9b2
laiapozo <pozotalarnlaia@gmail.com>
2025-03-14 10:46:49 +0100

    Render shows

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,5 @@
 /*
-    - Seleccionar botón y texto de la usuaria
+    - Seleccionar botón, texto de la usuaria, lista
     - Cuando la usuaria haga click en el botón:
         · Guardamos valor del input del texto
         · Hacer petición con el valor del input al final de la API
@@ -13,7 +13,24 @@
 
 const submitButton = document.querySelector(".js-submit-btn");
 const textInput = document.querySelector(".js-txt-search");
+const listShows = document.querySelector(".js-list-shows");
 let shows = [];
+
+const renderAnime = (shows) => {
+  for (const show of shows) {
+    const elementLi = document.createElement("li");
+    const imgLi = document.createElement("img");
+    const titleLi = document.createElement("h3");
+    elementLi.appendChild(imgLi);
+    elementLi.appendChild(titleLi);
+    listShows.appendChild(elementLi);
+
+    elementLi.classList.add(show.mal_id);
+    imgLi.setAttribute("src", show.images.jpg.image_url);
+    const contentTitle = document.createTextNode(show.title);
+    titleLi.appendChild(contentTitle);
+  }
+};
 
 const handleSearchClick = (ev) => {
   ev.preventDefault();
@@ -23,6 +40,7 @@ const handleSearchClick = (ev) => {
     .then((response) => response.json())
     .then((data) => {
       shows = data.data;
+      renderAnime(shows);
     });
 };
 
